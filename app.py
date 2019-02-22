d8c1a0af47636c2f04e6defd52bb28bdae8ccb57
Wojtek <wojtekj1989@gmail.com>
2019-02-22 13:52:43 +0100

    Created scaffold of flatbond endpoint

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+import validators
 
 app = Flask(__name__)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('rent', type=int)
+parser.add_argument('membership_fee', type=int)
+parser.add_argument('postcode', type=str)
+parser.add_argument('client_id', type=int)
 
 CONFIG = [
     {'id': 123, 'fixed_membership_fee': True, 'fixed_membership_amount': 15000}, 
@@ -25,9 +32,29 @@ class Config(Resource):
         else:
             return {'config': config[0]}
 
+class Flatbond(Resource):
+    def post(self):
+        args = parser.parse_args()
+        rent = args.get('rent')
+        membership_fee = args.get('membership_fee')
+        client_id = args.get('client_id')
+        postcode = args.get('postcode')
+
+        if any[!rent, !membership_fee, !client_id, !postcode]:
+            pass # TODO: handle missing data
+
+        postcode_valid = validators.validate_postcode(args["postcode"])
+        membership_fee_valid = validators.validate_fee(args["membership_fee"])
+
+        if any[!membership_fee_valid, !postcode_valid]:
+            pass # TODO: handle invalid data
+        
+        return {'status': 'OK', 'flatbond': args}
+
 api.add_resource(Hello, '/')
 api.add_resource(ConfigList, '/config')
 api.add_resource(Config, '/config/<client_id>')
+api.add_resource(Flatbond, '/flatbond')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
