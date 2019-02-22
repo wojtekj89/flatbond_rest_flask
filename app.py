c6dcf8d5f4c87eec825ab8cfeb5c40bc59769111
Wojtek <wojtekj1989@gmail.com>
2019-02-22 13:31:03 +0100

    adding endpoints for configs

--- app.py
+++ app.py
@@ -13,7 +13,21 @@ class Hello(Resource):
     def get(self):
         return {'msg': 'Hello World!'}
 
+class ConfigList(Resource):
+    def get(self):
+        return {'configs': CONFIG}
+
+class Config(Resource):
+    def get(self, client_id):
+        config = [item for item in CONFIG if item.get('id') == int(client_id)]
+        if len(config) == 0:
+            return {'msg': 'User not found'}, 404
+        else:
+            return {'config': config[0]}
+
 api.add_resource(Hello, '/')
+api.add_resource(ConfigList, '/config')
+api.add_resource(Config, '/config/<client_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
