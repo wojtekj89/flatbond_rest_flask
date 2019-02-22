fff440ed37ac7761757067f2d53bd4c9eee428ce
Wojtek <wojtekj1989@gmail.com>
2019-02-22 13:30:09 +0100

    Creating mock config data

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+CONFIG = [
+    {'id': 123, 'fixed_membership_fee': True, 'fixed_membership_amount': 15000}, 
+    {'id': 321, 'fixed_membership_fee': False, 'fixed_membership_amount': 25000},
+    ]
+
 class Hello(Resource):
     def get(self):
         return {'msg': 'Hello World!'}
