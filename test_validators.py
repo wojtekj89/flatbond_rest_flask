202338e352c6e818db84545e11fcbee3eeee3c1b
Wojtek <wojtekj1989@gmail.com>
2019-02-24 21:23:56 +0100

    updated unit tests

--- test_validators.py
+++ test_validators.py
@@ -10,13 +10,24 @@ class TestValidators(unittest.TestCase):
         self.assertTrue(validators.validate_postcode('B338TH')) 
         self.assertFalse(validators.validate_postcode('invalid')) 
         self.assertFalse(validators.validate_postcode('AAAAAAA'))
+        self.assertTrue(validators.validate_postcode("w42hg"))
 
     def test_member_fee(self):
         minimum = 120 * 1.2 * 100 # 120 GBP + 20% VAT in pences
-        self.assertTrue(validators.validate_fee(None, 100, minimum))
+        self.assertTrue(validators.validate_fee(None, 10000, minimum))
         self.assertFalse(validators.validate_fee({'fixed_membership_fee': True, 'fixed_membership_amount': 12000}, 50000, 12000))
+        self.assertTrue(validators.validate_fee(None, 12000, 14400))
         self.assertFalse(validators.validate_fee(None, 50000, minimum))
+        self.assertFalse(validators.validate_fee(None, 11100, 13320))
+        self.assertTrue(validators.validate_fee(None, 45454, 54545))
         self.assertTrue(validators.validate_fee({'fixed_membership_fee': False, 'fixed_membership_amount': 120}, 50000, 50000 * 1.2))
+
+    def test_rent(self):
+        self.assertFalse(validators.validate_rent(100))
+        self.assertTrue(validators.validate_rent(2500))
+        self.assertTrue(validators.validate_rent(3500))
+        self.assertTrue(validators.validate_rent(200000))
+        self.assertFalse(validators.validate_rent(200001))
 
 if __name__ == '__main__':
     unittest.main()
